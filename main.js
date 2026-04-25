185eba05acd631daedf003d3ee162ba4acd8207d
proGamer <81707598+insannaju@users.noreply.github.com>
2026-04-25 19:39:44 -0300

    Update main.js

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ function createLogin() {
   loginWin = new BrowserWindow({
     width: 400, height: 500,
     frame: false, resizable: false,
+    icon: path.join(__dirname, 'icon.ico'),
     webPreferences: { preload: path.join(__dirname, 'preload.js'), nodeIntegration: false, contextIsolation: true }
   })
   loginWin.loadFile('login.html')
@@ -22,6 +23,7 @@ function createMain() {
   mainWin = new BrowserWindow({
     width: 900, height: 620,
     frame: false, resizable: true,
+    icon: path.join(__dirname, 'icon.ico'),
     webPreferences: { preload: path.join(__dirname, 'preload.js'), nodeIntegration: false, contextIsolation: true }
   })
   mainWin.loadFile('index.html')
@@ -29,60 +31,43 @@ function createMain() {
 
 app.whenReady().then(createLogin)
 
-// Login
 ipcMain.on('do-login', (e, { user, pass }) => {
   if (user && pass) {
     loginWin.close()
     createMain()
   } else {
-    e.reply('login-error', 'Usuário ou senha inválidos')
+    e.reply('login-error', 'Usuario ou senha invalidos')
   }
 })
 
-// Fechar / minimizar
 ipcMain.on('close-app', () => { app.quit() })
-ipcMain.on('minimize-app', (e) => {
-  BrowserWindow.getFocusedWindow()?.minimize()
-})
+ipcMain.on('minimize-app', () => { BrowserWindow.getFocusedWindow()?.minimize() })
 
-// Listar scripts
 ipcMain.handle('list-scripts', () => {
   return fs.readdirSync(SCRIPTS_PATH).filter(f => f.endsWith('.lua'))
 })
-
-// Ler script
 ipcMain.handle('read-script', (e, name) => {
   return fs.readFileSync(path.join(SCRIPTS_PATH, name), 'utf8')
 })
-
-// Salvar script
 ipcMain.handle('save-script', (e, name, content) => {
   fs.writeFileSync(path.join(SCRIPTS_PATH, name), content, 'utf8')
   return true
 })
-
-// Novo script
 ipcMain.handle('new-script', (e, name) => {
   const file = path.join(SCRIPTS_PATH, name.endsWith('.lua') ? name : name + '.lua')
   fs.writeFileSync(file, '-- ' + name + '\n', 'utf8')
   return true
 })
-
-// Deletar script
 ipcMain.handle('delete-script', (e, name) => {
   fs.unlinkSync(path.join(SCRIPTS_PATH, name))
   return true
 })
-
-// Enviar pro PKM LAUNCHER
 ipcMain.handle('send-to-pkm', (e, name) => {
   const src = path.join(SCRIPTS_PATH, name)
   const dst = path.join(PKM_PATH, name)
   fs.copyFileSync(src, dst)
   return true
 })
-
-// Importar .lua externo
 ipcMain.handle('import-script', async () => {
   const { canceled, filePaths } = await dialog.showOpenDialog({
     filters: [{ name: 'Lua Scripts', extensions: ['lua'] }],
@@ -90,8 +75,7 @@ ipcMain.handle('import-script', async () => {
   })
   if (canceled) return []
   filePaths.forEach(fp => {
-    const name = path.basename(fp)
-    fs.copyFileSync(fp, path.join(SCRIPTS_PATH, name))
+    fs.copyFileSync(fp, path.join(SCRIPTS_PATH, path.basename(fp)))
   })
   return filePaths.map(fp => path.basename(fp))
 })
