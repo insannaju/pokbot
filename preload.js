b7e2ddfc00ab38b3057634ae2ecc1ca6aa64b494
proGamer <81707598+insannaju@users.noreply.github.com>
2026-04-26 19:36:59 -0300

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,9 +1,12 @@
-﻿const { contextBridge, ipcRenderer } = require('electron')
+const { contextBridge, ipcRenderer } = require('electron')
+
 contextBridge.exposeInMainWorld('api', {
   login: (user, pass) => ipcRenderer.send('do-login', { user, pass }),
   onLoginError: (cb) => ipcRenderer.on('login-error', (_, msg) => cb(msg)),
   close: () => ipcRenderer.send('close-app'),
   minimize: () => ipcRenderer.send('minimize-app'),
+  listProcesses: () => ipcRenderer.invoke('list-processes'),
+  selectClient: (proc) => ipcRenderer.send('select-client', proc),
   listScripts: () => ipcRenderer.invoke('list-scripts'),
   readScript: (name) => ipcRenderer.invoke('read-script', name),
   saveScript: (name, content) => ipcRenderer.invoke('save-script', name, content),
